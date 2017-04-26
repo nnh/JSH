1567153ca95845839a4b26a6fb8380c796d193dc
ma3k0 <mamiko.yonejima@nnh.go.jp>
2017-04-26 15:57:27 +0900

    2016年診断集計用施設別登録数

--- programs/JSH2017_jspho.dataset.R
+++ programs/JSH2017_jspho.dataset.R
@@ -4,19 +4,101 @@
 
 setwd("./rawdata")
 jspho <- read.csv("JSPHO_registration_160720_1501.csv", na.strings = c(""), as.is=T, fileEncoding="CP932")
+jsh <- read.csv("JSH_report_160623_1459.csv", as.is=T, fileEncoding="CP932")
+jsh.rgst <- read.csv("JSH_registration_160623_1459.csv", as.is=T, fileEncoding="CP932")
+jspho <- read.csv("JSPHO_registration_160720_1501.csv", as.is=T, fileEncoding="CP932")
+nhoh <- read.csv("NHOH_report_161018_1705.csv", as.is=T, fileEncoding="CP932")
+nhoh.rgst <- read.csv("NHOH_registration_161018_1705.csv", as.is=T, fileEncoding="CP932")
 
 setwd("../input")
-disease <- read.csv("disease.csv", fileEncoding="UTF-8-BOM", header=F, as.is=T)  # fileEncoding="UTF-8-BOM"付けると読み込めません
+disease <- read.csv("disease.csv", fileEncoding="UTF-8-BOM", header=F, as.is=T)  
 colnames(disease) <- c("大分類", "MHDECOD", "病名略", "MHTERM", "病名英語", "MHDECOD+0", "中分類番号", "中分類略名" ,"中分類名日本語" ,"中分類名英語", "tumor_or_nontumor", "中分類番号")
+facilities <- read.csv("facilities.csv", fileEncoding="UTF-8-BOM", header=T, as.is=T)  
 
 source("../programs/jsphotowho.R", chdir=F, encoding="UTF-8")
 source("../programs/jsphotowho_nontumor.R", chdir=F, encoding="UTF-8")
 
-dataset.jspho <- rbind(jspho.1, jspho.non.t.1)  # 腫瘍性と非腫瘍性をくっつける
-dxt.jspho <- jspho[, c(15, 39:286)]  # 詳細をくっつける
+#施設コードをマージする処理(NHOH)
+p.nhoh.rgst <- nhoh.rgst[,c("登録コード","初発時住所")]
+p.nhoh.rgst$SCSTRESC <- floor(as.integer(sub("^.*.-","",p.nhoh.rgst$初発時住所))/1000)
+m.nhoh <- merge(nhoh,p.nhoh.rgst, by="登録コード", all.x= T)
 
-ads.jspho <- merge(dataset.jspho, dxt.jspho,  by.x = "SUBJID", by.y = "登録コード", all.x = T)
-ads.jspho[is.na(ads.jspho)] <- ""
-setwd("../output")
-write.csv(ads.jspho, "Mappig.data.JSPHO_for_JSH.csv", row.names = F)
-setwd("..")
+# 診断年月日2012年以降、腫瘍性病変のみを抽出
+nhoh.1 <- m.nhoh[as.integer(substr(m.nhoh$診断年月日, 1, 4)) > 2011 & as.integer(substr(m.nhoh$診断年月日, 1, 4)) <= 2016 & (m.nhoh$field2 < 1000),
+                 c("登録コード", "性別", "SCSTRESC", "生死", "死亡日", "最終確認日", "シート作成時施設コード", "field2",
+                   "確定診断名", "生年月日", "診断年月日")] 
+colnames(nhoh.1)[1:11] <- c("SUBJID", "SEX", "SCSTRESC", "DTHFL", "DTHDTC", "DSSTDTC", "SITEID", "MHDECOD", "MHTERM",
+                            "BRTHDTC", "MHSTDTC")
+
+nhoh.1$STUDYID <- "NHOH"
+
+#施設コードをマージする処理(JSH)
+p.jsh.rgst <- jsh.rgst[,c("登録コード","初発時住所")]
+p.jsh.rgst$SCSTRESC <- floor(as.integer(sub("^.*.-","",p.jsh.rgst$初発時住所))/1000)
+m.jsh <- merge(jsh, p.jsh.rgst, by="登録コード", all.x= T)
+
+# 診断年月日2012年以降、腫瘍性病変のみを抽出
+jsh.1 <- m.jsh[as.integer(substr(m.jsh$診断年月日, 1, 4)) > 2011 & as.integer(substr(m.jsh$診断年月日, 1, 4)) <= 2016 & (m.jsh$field1 < 1000),
+               c("登録コード", "性別", "SCSTRESC", "生死", "死亡日", "最終確認日", "シート作成時施設コード", "field1",
+                 "確定診断名", "生年月日", "診断日")]
+colnames(jsh.1)[1:11] <- c("SUBJID", "SEX", "SCSTRESC", "DTHFL", "DTHDTC", "DSSTDTC", "SITEID", "MHDECOD", "MHTERM",
+                           "BRTHDTC", "MHSTDTC")
+jsh.1$STUDYID <- "JSH"
+
+dataset.3org <- rbind(jsh.1, nhoh.1, jspho.1, jspho.non.t.1)  # 3団体を繋げた基本のデータセットを作成
+dataset.3org$age.diagnosis <- YearDif(dataset.3org$BRTHDTC, dataset.3org$MHSTDTC)
+
+dxt.dataset.3org.year <- dataset.3org[substr(dataset.3org$MHSTDTC, 1, 4) == 2016, ]  # 診断年のみ抽出 
+dxt.dataset.3org.year$count <- 1
+
+# 団体別登録数
+# 施設数
+by.org.c.facilities <- xtabs( ~ SITEID + STUDYID , data = dxt.dataset.3org.year )
+by.org.c.facilities.df <- as.data.frame(by.org.c.facilities)
+by.org.c.facilities.df$count <- ifelse(by.org.c.facilities.df$Freq == 0, 0, 1)
+by.org.facilities <- xtabs( ~ STUDYID + count , data = by.org.c.facilities.df )
+by.org.facilities.mat <- matrix(by.org.facilities , nrow(by.org.facilities), ncol(by.org.facilities))
+rownames(by.org.facilities.mat) <- rownames(by.org.facilities)
+colnames(by.org.facilities.mat) <- c("登録なし", "施設数")
+# 登録数
+by.org.np <- xtabs( ~ STUDYID + count , data = dxt.dataset.3org.year )
+by.org.mat <- matrix(by.org.np, nrow(by.org.np), ncol(by.org.np))
+rownames(by.org.mat) <- rownames(by.org.np)
+colnames(by.org.mat) <- "登録数"
+# 施設数と登録数をつなぐ
+by.org <- cbind(by.org.facilities.mat, by.org.mat)
+by.organization <- by.org[, c(2,3)]
+
+res.by.organization <- data.frame(apply(by.organization, 2, function(d){ c(d, sum(d))}))  # 総計の行追加
+
+# 施設別登録数
+by.facilities <- xtabs( ~ SITEID + count , data = dxt.dataset.3org.year)
+by.facilities.df <- as.data.frame(by.facilities)
+by.facilities.df <- by.facilities.df[, c(1, 3)]
+colnames(by.facilities.df)[2] <- "登録数"
+by.facilities.np <- merge(by.facilities.df, facilities, by.x = "SITEID", by.y = "施設CD", all.x = T)
+dxt.by.facilities <- by.facilities.np[, c(1, 3, 2)]
+colnames(dxt.by.facilities)[2] <- "施設名"
+
+total <- data.frame(
+  SITEID = "00000",
+  施設名 = "合計", 
+  登録数 = sum(dxt.by.facilities$登録数)
+)  # 合計の計算
+
+res.by.facilities <- rbind(dxt.by.facilities, total)
+
+# # Markdownにしようか。。。
+# library(formattable)
+# res.by.organization -> temp
+# formattable::formattable(temp)
+# res.by.facilities -> temp
+# formattable::formattable(temp)
+
+# dxt.jspho <- jspho[, c(15, 39:286)]  # 詳細をくっつける
+# 
+# ads.jspho <- merge(dataset.jspho, dxt.jspho,  by.x = "SUBJID", by.y = "登録コード", all.x = T)
+# ads.jspho[is.na(ads.jspho)] <- ""
+# setwd("../output")
+# write.csv(ads.jspho, "Mappig.data.JSPHO_for_JSH.csv", row.names = F)
+# setwd("..")
