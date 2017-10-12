9e6841305f11404d9ce758abbb39ed6dfc80864a
Kirimichan <mariko.ohtsuka@nnh.go.jp>
2017-10-12 16:30:05 +0900

    20171012 mod

--- programs/piechart.R
+++ programs/piechart.R
@@ -2,82 +2,119 @@
 # Mariko Ohtsuka
 # 2017/10/10
 # package : sas7bdat
-
 # Import library
 library(sas7bdat)
-# 世代AGECAT2N  1:child,2:aya,3:adult,4:old, all列に合計を入れる
-cstGene.lst <- c("child","aya","adult","old","all")
-cstGene.cnt <- length(cstGene.lst)
-# グラフの色をセット
-graph.col.lst <- rainbow(12)
+
+# Constant section
+# 世代(AGECAT2N)  1:child,2:aya,3:adult,4:old, all列に合計を入れる
+kGenelst <- c("child","aya","adult","old","all")
+gene_cnt <- length(kGenelst)
+# グラフの色をセット 上位11病名+合計
+kRainbow <- rainbow(12)
+# rainbowから1赤、4緑、8ロイヤルブルー、3黄、12マゼンタ、6緑、9青、2オレンジ、11赤紫、5緑、7水色、10青紫
+kGraph_color <- c(kRainbow[1], kRainbow[4], kRainbow[8], kRainbow[3], kRainbow[12], kRainbow[6], kRainbow[9], kRainbow[2], kRainbow[11], kRainbow[5], kRainbow[7], kRainbow[10])
+# 実行環境がmacかwindowsか
+kOs_mac <- 0
+kOs_win <- 1
+# todo 実行環境情報取得
+# getenv_platform <- Sys.getenv("R_PLATFORM")
+# if (grep(getenv_platform, pattern="apple") == 1) {
+#   # mac
+#   platform_f <- kOs_mac
+# } elseif (grep(getenv_platform, pattern="windows") == 1) {
+#   # windows
+#   platform_f <- kOs_win
+# }
+platform_f <- kOs_win
 
 # InputData path
-sasdatprt.path <- "//aronas/Stat/Trials"
-sasdatads.path <- "JSH2017/ADS"
-sasdat.nm <- "ads.sas7bdat"
-sasdat.path <- paste(sasdatprt.path, sasdatads.path, sasdat.nm, sep="/")
+sasdat_parent_path <- "//aronas/Stat/Trials"
+sasdat_ads.folder <- "JSH2017/ADS"
+sasdat_name <- "ads.sas7bdat"
+sasdat_path <- paste(sasdat_parent_path, sasdat_ads.folder, sasdat_name, sep="/")
+# OutputData path
+output_path <- "C:/Users/MarikoOhtsuka/Desktop/plot/"
+if (platform_f == kOs_mac) {
+  output_ext <- ""
+} else {
+  output_ext <- ".xmf"
+}
+# READ SASOutputData
+sasdat <- read.sas7bdat(sasdat_path)
 
-# READ SAS Output Data
-sasdat <- read.sas7bdat(sasdat.path)
-# 件数集計用
-sasdat$wk.sum <- rep(1:1, nrow(sasdat))
-
-# データから疾患群リスト作成
-MHGRPTERM.lst <- levels(sasdat$MHGRPTERM)
-# for (i in 1:length(MHGRPTERM.lst)) {
-  i <- 1
-  # 疾患群ごとにデータ分け
-  wk.sasdat <- subset(sasdat, MHGRPTERM==MHGRPTERM.lst[i])
-  # 疾患群ごとに詳細病名リストが変更になる
-  MHTERM.lst <- levels(factor(wk.sasdat$MHTERM))
-  # 世代ごとの列を作成したデータフレーム
-  wk2.ds <- data.frame(matrix(rep(NA, cstGene.cnt), nrow=1))[numeric(0), ]
-  colnames(wk2.ds) <- cstGene.lst
-  for (k in 1:cstGene.cnt){
-  # 世代ごとに集計
-    wk.gene <- cstGene.lst[k]
-    for (j in 1:length(MHTERM.lst)){
-      # 詳細病名ごとに件数集計
-      wk.disease <- MHTERM.lst[j]
-      if (k!=cstGene.cnt) {
-        wk3.ds <- subset(wk.sasdat, ((MHTERM==wk.disease) & (AGECAT2N==k)))
-
-      } else {
-        # ALLに合計を格納
-        wk3.ds <- subset(wk.sasdat, MHTERM==wk.disease)
-      }
-      wk2.ds[wk.disease, wk.gene] <- sum(wk3.ds$wk.sum)
+# InputDataから疾患群リスト作成
+mhgrpterm_lst <- levels(sasdat$MHGRPTERM)
+# for (i in 1:length(mhgrpterm_lst)) {
+i <- 1
+# 疾患群ごとにデータ分け
+wk_sasdat <- subset(sasdat, MHGRPTERM == mhgrpterm_lst[i])
+# 疾患群ごとに詳細病名リストが変更になるので入れなおす
+mhterm_lst <- levels(factor(wk_sasdat$MHTERM))
+# 世代ごとの列を作成した空のデータフレームを作成
+dst_gene_mhterm <- data.frame(matrix(rep(NA, gene_cnt), nrow=1))[numeric(0), ]
+colnames(dst_gene_mhterm) <- kGenelst
+# 世代ごとに集計
+for (k in 1:gene_cnt){
+  # 詳細病名ごとに件数集計
+  for (j in 1:length(mhterm_lst)){
+    wk_disease <- mhterm_lst[j]
+    if (k!=gene_cnt) {
+      # 各世代の列は世代ごとに集計
+      dst_wk_sum <- subset(wk_sasdat, ((MHTERM == wk_disease) & (AGECAT2N == k)))
+    } else {
+      # ALL列に合計を格納
+      dst_wk_sum <- subset(wk_sasdat, MHTERM == wk_disease)
     }
+    # dst_gene_mhterm[wk_disease, kGenelst[k]] <- sum(dst_wk_sum$wk_sum)
+    dst_gene_mhterm[wk_disease, kGenelst[k]] <- nrow(dst_wk_sum)
   }
-  # 世代ALLの詳細病名件数の降順にソートし、色を決定
-  # 上位11＋その合計の表を作成
-  # Sort(desc)
-  srtKey <- cstGene.lst[cstGene.cnt]
-  wk.Srtlist <- order(wk2.ds[[srtKey]], decreasing=T)
-  wk4.ds <- wk2.ds[wk.Srtlist, ]
-  wk4.ds$graphcolor <- graph.col.lst[1:nrow(wk4.ds)]
-  wk4.ds$wk.per <- NA
-  wk4.ds$wk.lbl <- NA
-  # グラフ生成
-#  par(mfrow = c(1, cstGene.cnt))
-#  test <- paste("C:/Users/MarikoOhtsuka/Desktop/plot",9 ,".png", sep="")
-#  png(test, width = 2000, height = 400)  # 描画デバイスを開く
-  #par(mai=c(0,0,0,0)) #余白
-  #par(xpd=F,pty="s")
-  #par(mgp=c(-10,-10,-10))
-  for (m in 1:cstGene.cnt){
-    test <- paste("C:/Users/MarikoOhtsuka/Desktop/plot",m ,".png", sep="")
-    png(test, width = 500, height = 500)  # 描画デバイスを開く
-    # ラベル作成
-    wk.denom <- sum(wk4.ds[,m])
-    wk4.ds$wk.per <- floor(((wk4.ds[,m] / wk.denom) * 100) + 0.5)
-    wk4.ds$wk.lbl <- paste(wk4.ds$wk.per, "%")
-    pie(wk4.ds[,m], label=wk4.ds$wk.lbl, main=colnames(wk4.ds[m]),cex.main=2, col=wk4.ds$graphcolor, radius=0.5)
-    if (m==3) {
-      legend("bottom",legend=rownames(wk4.ds), fill=wk4.ds$graphcolor, ncol=4)
-    }
-    dev.off()
+}
+# 世代ALLの詳細病名件数の降順にソートし、色を決定
+# 上位11＋その合計の表を作成
+sort_key <- kGenelst[gene_cnt]
+wk_sortlist <- order(dst_gene_mhterm[[sort_key]], decreasing=T)
+dst_piechart <- dst_gene_mhterm[wk_sortlist, ]
+if (nrow(dst_piechart) > 11) {
+  dst_piechart <- dst_piechart[1:11]
+}
+# パイチャート設定色をセット
+dst_piechart$graph_color <- kGraph_color[1:nrow(dst_piechart)]
+
+# 各項目のパーセンテージラベル作成作業用列
+dst_piechart$wk_per <- NA
+dst_piechart$wk_lbl <- NA
+# グラフ生成
+graphics.off()
+
+par(mai=c(0, 0, 0, 0)) #余白
+par(omi=c(0, 0, 0, 0))
+for (m in 1:gene_cnt){
+  wk_disease_list <- rownames(dst_piechart)
+  # 疾患群名+連番でファイル名を生成
+  # 禁止文字の除去
+  # todo 正規表現で他のも
+  wk_filename <- gsub("/", "", wk_disease_list[i])
+  output_filename <- paste(output_path, wk_filename, "_", m, output_ext, sep="")
+  # メタファイルに出力
+  if (platform_f == kOs_mac) {
+    # todo mac
+  } else {
+    win.metafile(filename=output_filename)
   }
-#  dev.off()
+    # 各項目のパーセンテージラベル作成
+  wk_denom <- sum(dst_piechart[,m])
+  dst_piechart$wk_per <- floor(((dst_piechart[ ,m] / wk_denom) * 100) + 0.5)
+  dst_piechart$wk_lbl <- paste(dst_piechart$wk_per, "%")
+  # パイチャート出力
+  pie(dst_piechart[ ,m], label=dst_piechart$wk_lbl, col=dst_piechart$graph_color, radius=0.5)
+  legend("bottom", legend=wk_disease_list, fill=dst_piechart$graph_color, ncol=3)
+  # 世代名を上部に出力
+  par(new=T)
+  text(0,0.7,colnames(dst_piechart[m]))
+  par(new=F)
+  dev.off()
+}
+
+
 #}
 
